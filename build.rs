42c12f826b9443c635b48d93af9829c81f78bcfb
Carl Andersson <carl@carlandersson.net>
2022-12-07 05:16:38 +0100

    Fix Mac OS compilation - Clang needs c++14 defined (#2)

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() -> miette::Result<()> {
     autocxx_build::Builder::new("src/lib.rs", &["vqf/vqf/cpp/"])
         .build()?
         .file("vqf/vqf/cpp/vqf.cpp")
+        .flag_if_supported("-std=c++14")
         .compile("autocxx-vqf");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
